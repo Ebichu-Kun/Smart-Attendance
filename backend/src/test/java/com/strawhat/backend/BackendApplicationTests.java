7e96aaf80fa8ce13fe05a3ec611ff4dc8b52d7a6
Ebichu-kun <ghanshyampardeshi29@gmail.com>
2026-08-28 19:44:50 +0530

    project structure

--- backend/src/test/java/com/strawhat/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/strawhat/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.strawhat.smartattendance;
+package com.strawhat.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SmartattendanceApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
